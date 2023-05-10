d723e6ebfc6dab6d3d5e66c3f8c2bb6e40744cc9
RohendoJ <rohendoj3134.github@gmail.com>
2023-05-10 07:17:45 +0700

    update form

--- src/pages/EditBook.jsx
+++ src/pages/EditBook.jsx
@@ -21,7 +21,9 @@ const EditBook = () => {
 
   const docSnap = doc(db, "books", id);
 
-  const updateBooks = async () => {
+  const updateBooks = async (e) => {
+    e.preventDefault();
+
     try {
       setIsProcessing(true);
       const newFields = {
@@ -57,99 +59,100 @@ const EditBook = () => {
 
   return (
     <div>
-      <div className="grid place-items-center min-[350px]:mt-10 mt-10">
-        <h1 className="text-teal-500 font-bold text-[2.5rem]">Modify Book</h1>
-      </div>
-      <h2 className="pl-10 mt-[2rem] mb-2 items-start md:pl-[25vw] lg:pl-[30vw] xl:pl-[35vw] 2xl:pl-[35vw]">
-        Nama Buku
-      </h2>
-      <div className="grid place-items-center">
-        <input
-          type="text"
-          value={nameBook}
-          className="w-[80vw] h-[40px] bg-slate-100 rounded pl-3 outline-none focus:outline-teal-300 duration-500 md:w-[50vw] lg:w-[40vw] xl:w-[30vw] 2xl:w-[30vw]"
-          placeholder="Edit nama buku"
-          onChange={(event) => {
-            setNameBook(event.target.value);
-          }}
-          required
-        />
-      </div>
-      <h2 className="pl-10 mt-[1rem] mb-2 items-start md:pl-[25vw] lg:pl-[30vw] xl:pl-[35vw] 2xl:pl-[35vw]">
-        Penerbit
-      </h2>
-      <div className="grid place-items-center">
-        <input
-          type="text"
-          value={publisher}
-          className="w-[80vw] h-[40px] bg-slate-100 rounded pl-3 outline-none focus:outline-teal-300 duration-500 md:w-[50vw] lg:w-[40vw] xl:w-[30vw] 2xl:w-[30vw]"
-          placeholder="Edit penerbit"
-          onChange={(event) => {
-            setPublisher(event.target.value);
-          }}
-          required
-        />
-      </div>
-      <h2 className="pl-10 mt-[1rem] mb-2 items-start md:pl-[25vw] lg:pl-[30vw] xl:pl-[35vw] 2xl:pl-[35vw]">
-        Penulis
-      </h2>
-      <div className="grid place-items-center">
-        <input
-          type="text"
-          value={author}
-          className="w-[80vw] h-[40px] bg-slate-100 rounded pl-3 outline-none focus:outline-teal-300 duration-500 md:w-[50vw] lg:w-[40vw] xl:w-[30vw] 2xl:w-[30vw]"
-          placeholder="Edit penulis"
-          onChange={(event) => {
-            setAuthor(event.target.value);
-          }}
-          required
-        />
-      </div>
-      <h2 className="pl-10 mt-[1rem] mb-2 items-start md:pl-[25vw] lg:pl-[30vw] xl:pl-[35vw] 2xl:pl-[35vw]">
-        Jumlah halaman
-      </h2>
-      <div className="grid place-items-center">
-        <input
-          type="number"
-          value={numberOfPages}
-          className="w-[80vw] h-[40px] bg-slate-100 rounded pl-3 outline-none focus:outline-teal-300 duration-500 md:w-[50vw] lg:w-[40vw] xl:w-[30vw] 2xl:w-[30vw]"
-          placeholder="Edit jumlah halaman"
-          onChange={(event) => {
-            setNumberOfPages(event.target.value);
-          }}
-          min={1}
-          required
-        />
-      </div>
-      <div className="flex gap-3 mt-5 justify-end pr-10 md:pr-[25vw] lg:pr-[30vw] xl:pr-[35vw] 2xl:pr-[35vw]">
-        <Link to="/" className="text-teal-500 mt-1">
-          Cancel
-        </Link>
-        <button
-          className={`bg-teal-500 rounded text-white px-2 py-1 ${
-            (!validNameBooks ||
+      <form onSubmit={updateBooks}>
+        <div className="grid place-items-center min-[350px]:mt-10 mt-10">
+          <h1 className="text-teal-500 font-bold text-[2.5rem]">Modify Book</h1>
+        </div>
+        <h2 className="pl-10 mt-[2rem] mb-2 items-start md:pl-[25vw] lg:pl-[30vw] xl:pl-[35vw] 2xl:pl-[35vw]">
+          Nama Buku
+        </h2>
+        <div className="grid place-items-center">
+          <input
+            type="text"
+            value={nameBook}
+            className="w-[80vw] h-[40px] bg-slate-100 rounded pl-3 outline-none focus:outline-teal-300 duration-500 md:w-[50vw] lg:w-[40vw] xl:w-[30vw] 2xl:w-[30vw]"
+            placeholder="Edit nama buku"
+            onChange={(event) => {
+              setNameBook(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <h2 className="pl-10 mt-[1rem] mb-2 items-start md:pl-[25vw] lg:pl-[30vw] xl:pl-[35vw] 2xl:pl-[35vw]">
+          Penerbit
+        </h2>
+        <div className="grid place-items-center">
+          <input
+            type="text"
+            value={publisher}
+            className="w-[80vw] h-[40px] bg-slate-100 rounded pl-3 outline-none focus:outline-teal-300 duration-500 md:w-[50vw] lg:w-[40vw] xl:w-[30vw] 2xl:w-[30vw]"
+            placeholder="Edit penerbit"
+            onChange={(event) => {
+              setPublisher(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <h2 className="pl-10 mt-[1rem] mb-2 items-start md:pl-[25vw] lg:pl-[30vw] xl:pl-[35vw] 2xl:pl-[35vw]">
+          Penulis
+        </h2>
+        <div className="grid place-items-center">
+          <input
+            type="text"
+            value={author}
+            className="w-[80vw] h-[40px] bg-slate-100 rounded pl-3 outline-none focus:outline-teal-300 duration-500 md:w-[50vw] lg:w-[40vw] xl:w-[30vw] 2xl:w-[30vw]"
+            placeholder="Edit penulis"
+            onChange={(event) => {
+              setAuthor(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <h2 className="pl-10 mt-[1rem] mb-2 items-start md:pl-[25vw] lg:pl-[30vw] xl:pl-[35vw] 2xl:pl-[35vw]">
+          Jumlah halaman
+        </h2>
+        <div className="grid place-items-center">
+          <input
+            type="number"
+            value={numberOfPages}
+            className="w-[80vw] h-[40px] bg-slate-100 rounded pl-3 outline-none focus:outline-teal-300 duration-500 md:w-[50vw] lg:w-[40vw] xl:w-[30vw] 2xl:w-[30vw]"
+            placeholder="Edit jumlah halaman"
+            onChange={(event) => {
+              setNumberOfPages(event.target.value);
+            }}
+            min={1}
+            required
+          />
+        </div>
+        <div className="flex gap-3 mt-5 justify-end pr-10 md:pr-[25vw] lg:pr-[30vw] xl:pr-[35vw] 2xl:pr-[35vw]">
+          <Link to="/" className="text-teal-500 mt-1">
+            Cancel
+          </Link>
+          <button
+            className={`bg-teal-500 rounded text-white px-2 py-1 ${
+              (!validNameBooks ||
+                !validPublisher ||
+                !validAuthor ||
+                !validNumberOfPages) &&
+              "cursor-not-allowed opacity-50"
+            } `}
+            disabled={
+              !validNameBooks ||
               !validPublisher ||
               !validAuthor ||
-              !validNumberOfPages) &&
-            "cursor-not-allowed opacity-50"
-          } `}
-          onClick={updateBooks}
-          disabled={
-            !validNameBooks ||
-            !validPublisher ||
-            !validAuthor ||
-            !validNumberOfPages
-          }
-        >
-          {isProcessing ? (
-            <div className="px-8">
-              <FontAwesomeIcon icon="fa-solid fa-circle-notch" spin />
-            </div>
-          ) : (
-            "Add a book"
-          )}
-        </button>
-      </div>
+              !validNumberOfPages
+            }
+          >
+            {isProcessing ? (
+              <div className="px-8">
+                <FontAwesomeIcon icon="fa-solid fa-circle-notch" spin />
+              </div>
+            ) : (
+              "Add a book"
+            )}
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
